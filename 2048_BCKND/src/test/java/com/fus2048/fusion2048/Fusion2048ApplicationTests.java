ee692b198040c27890db30305aa74c23b0862d90
Om Sood <112969671+0m12s@users.noreply.github.com>
2026-02-18 00:59:20 +0530

    Add files via upload

---
+++ 2048_BCKND/src/test/java/com/fus2048/fusion2048/Fusion2048ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fus2048.fusion2048;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fusion2048ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
