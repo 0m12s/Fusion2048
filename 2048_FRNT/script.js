e1d741962ad46abfc6e8ac854913287fe29acbab
Om Sood <112969671+0m12s@users.noreply.github.com>
2026-02-18 06:56:28 +0530

    Update script.js

--- 2048_FRNT/script.js
+++ 2048_FRNT/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://fusion2048.onrender.com/api";
+const API_BASE = "https://fusion2048.onrender.com";
 const GAME_API = `${API_BASE}/game`;
 
 let gameState = null;
@@ -306,6 +306,7 @@ function logout() {
   localStorage.removeItem("token");
   location.reload();
 }
+
 
 
 
